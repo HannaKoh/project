87ba6ebc41b9a848991f8b5744aaa02cb5a99a11
Anna <AnnaKohanovskaya68@gmail.com>
2021-09-07 17:36:53 +0300

    NavLink

--- proces/src/components/Navbar/Navbar.jsx
+++ proces/src/components/Navbar/Navbar.jsx
@@ -8,19 +8,19 @@ const Navbar = () => {
         (
         <nav className={s.nav}>
             <div className = {s.item}>
-                <NavLink to='Profile' activeClassName={s.activeLink}>Profile</NavLink>
+                <NavLink to='/Profile' activeClassName={s.activeLink}>Profile</NavLink>
             </div>
             <div className = {s.item}>
-                <NavLink to='Dialogs' activeClassName={s.activeLink}>Messages</NavLink>
+                <NavLink to='/Dialogs' activeClassName={s.activeLink}>Messages</NavLink>
             </div>
             <div className = {s.item}>
-                <NavLink to='News' activeClassName={s.activeLink}>News</NavLink>
+                <NavLink to='/News' activeClassName={s.activeLink}>News</NavLink>
             </div>
             <div className = {s.item}>
-                <NavLink to = 'Music' activeClassName={s.activeLink}>Music</NavLink>
+                <NavLink to = '/Music' activeClassName={s.activeLink}>Music</NavLink>
             </div>
             <div className = {s.item}>
-                <NavLink to='Settings' activeClassName={s.activeLink}>Settings</NavLink>
+                <NavLink to='/Settings' activeClassName={s.activeLink}>Settings</NavLink>
             </div>
         </nav>
         )
