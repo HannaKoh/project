b22411e2c2d5c3c7c68f236a1673bfe281786b82
Anna <AnnaKohanovskaya68@gmail.com>
2021-09-08 17:58:38 +0300

    map

--- proces/src/components/Dialogs/Dialogs.jsx
+++ proces/src/components/Dialogs/Dialogs.jsx
@@ -16,7 +16,7 @@ const Message = (props) =>{
 
 const Dialogs = (props) => {
 
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Hanna'},
         {id: 2, name: 'Nikolay'},
         {id: 3, name: 'Andrey'},
@@ -24,25 +24,31 @@ const Dialogs = (props) => {
         {id: 5, name: 'Alex'}
     ]
 
-    let messageData = [
+    let messages = [
         {id: 1, message:'Hi! How are you?'},
         {id: 2, message:'How is your job? Finished the redesign?'},
         {id: 3, message:'Maybe we will meet in the evening?'}
     ]
 
+    let dialogsElements = dialogs.map( (d) => <DialogItem name = {d.name} id = {d.id} /> );
+
+    let messagesElements = messages.map( m => <Message m = {m.message} />)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name = {dialogsData[0].name} id = {dialogsData[0].id}/>
+                {dialogsElements}
+                {/*<DialogItem name = {dialogsData[0].name} id = {dialogsData[0].id}/>
                 <DialogItem name = {dialogsData[1].name} id = {dialogsData[1].id}/>
                 <DialogItem name = {dialogsData[2].name} id = {dialogsData[2].id}/>
                 <DialogItem name = {dialogsData[3].name} id = {dialogsData[3].id}/>
-                <DialogItem name = {dialogsData[4].name} id = {dialogsData[4].id}/>
+                <DialogItem name = {dialogsData[4].name} id = {dialogsData[4].id}/>*/}
             </div>
             <div className={s.messages}>
-                <Message message = {messageData[0].message} />
+                {messagesElements}
+               {/* <Message message = {messageData[0].message} />
                 <Message message = {messageData[1].message} />
-                <Message message = {messageData[2].message} />
+                <Message message = {messageData[2].message} />*/}
             </div>
         </div>
     )
